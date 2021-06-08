6cf207d3a90370c4b9e77be7c0a353beab82ab4e
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-06-08 11:36:39 -0300

    cree componente Catalogo + action getProductos

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { Home } from './Components/Home';
 import Nav from './Components/Nav/Nav';
+import{Catalogo} from "./Components/Catalogo/Catalogo"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Switch>
-        <Route path='/' component={Home}/>
+        <Route path='/' component={Catalogo}/>
       </Switch>
     </div>
   );
