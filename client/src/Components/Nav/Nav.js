ae0b7cbc1bae54cea5f3dfd2dbb75f1d680e74bb
Sebastian <tassone_af@yahoo.com>
2021-06-08 00:18:34 -0300

    adding icons

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import  './Nav.css';
+import { NavLink } from 'react-router-dom';
+import './Nav.css';
+import SearchBar from './SearcBar/SearchBar';
 
-export default function Nav(){
+export default function Nav() {
 
     return (
         <div className='nav'>
-
+            <nav className='nav'>
+                <NavLink to='/home' className='navLink'>🏠 Home</NavLink>
+                <NavLink to='/contact' className='navLink'>👤</NavLink>
+                <SearchBar />
+            </nav>
         </div>
     )
 }
--- client/src/Components/Nav/SearcBar/SearchBar.js
+++ client/src/Components/Nav/SearcBar/SearchBar.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
 export default function SearchBar() {
 
+
+    const [input, setInput] = useState('');
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        document.getElementById('searchBar').reset()
+
+    };
+
     return (
         <div>
-
+            <form className='searchBar' onSubmit={handleSubmit}>
+                <input className='searchInput' type="text" placeholder="Search..." onChange={handleInputChange} />
+                <input className='searchBtn' type="submit" value='Search 🔍' />
+            </form>
         </div>
     )
 }
