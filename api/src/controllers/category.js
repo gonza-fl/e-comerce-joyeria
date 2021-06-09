b2ffbbc60a91052169354c5cf09703da30724bc6
=Maximiliano <scriptnoob12@gmail.com>
2021-06-09 12:03:44 -0300

    Se integro la img al modelo categoria y a la ruta de creacion de esta misma

--- api/src/controllers/category.js
+++ api/src/controllers/category.js
@@ -2,19 +2,27 @@ const { Category } = require('../models/index');
 
 const addCategory = async (req, res) => { 
 
-  const { name, description } = req.body;
+  const { name, description,img } = req.body;
   if (!name || name.trim().length === 0) {
     return res.json({err:'El nombre de la categoria no puede ser vacia'});
   }
   if (!description || description.trim().length === 0) {
     return res.json({err:'La descripción de la categoria no puede ser vacia'});
   }
+  if (!img) {
+    return res.json({err:'La imagen de la categoria no puede ser vacia'});
+  }
   
   try {
     const [category, created] = await Category.findOrCreate({ 
       where: { name: name.trim() }, 
-      defaults: { name: name.trim(), description: description.trim() }
-    });        
+      defaults: { 
+        name: name.trim(), 
+        description: description.trim(),
+        img: "https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg" 
+      }
+    });
+    //HASTA QUE ESTE CONECTADA LA API DE IMG, se usa una URL ESTATICA        
     if (created) {
       return res.json({success: 'La categoria ha sido creada!'});
     } 
@@ -25,10 +33,10 @@ const addCategory = async (req, res) => {
   }
 }
 
-const getCategory = async (req,res) =>{
+const getCategory = async (_req,res) =>{
   try {
     await Category.findAll({
-      attributes:['id','name','img']
+      attributes:['id','name','img','description']
     }).then((result)=>{
       return res.status(201).json(result);
     })
