42268e8ed2fc866fa4e62c8a0b010bb25867e833
Sebastian <tassone_af@yahoo.com>
2021-06-09 01:13:55 -0300

    CardFilter use props

--- client/src/Components/Filters/CardFilter/CardFilter.js
+++ client/src/Components/Filters/CardFilter/CardFilter.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './CardFilter.css'
 
 
-export default function CardFilter(){
+export default function CardFilter({id, name, img}){
 
     return (
         <div>
-            
+            <div>
+                <p>{name}</p>
+            </div>
+            <Link to={'#'+id}> 
+                <img src={img} alt={name} />
+            </Link>
         </div>
     )
 }
