d586f5d579bfa14e7bd011542800432d81961f2d
Sebastian <tassone_af@yahoo.com>
2021-06-09 00:54:19 -0300

    Created fake data base for rendering

---
+++ client/src/Components/fakeDB-Categories.js
@@ -0,0 +1,40 @@
+export const categories = [
+    {
+        id:1,
+        name: 'Anillos',
+        img: 'https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'
+    },
+    {
+        id:2,
+        name: 'Collares',
+        img: 'https://static.ella-hoy.es/ellahoy/fotogallery/780X0/37755/collares-verano-2012-dorado-con-colgantes.jpg'
+    },
+    {
+        id:3,
+        name: 'Aretes',
+        img: 'https://static.ella-hoy.es/ellahoy/fotogallery/780X0/480607/pendientes-de-aro-tous-bear-de-oro.jpg'
+    },
+    {
+        id:4,
+        name: 'Earcuffs',
+        img: 'https://i.pinimg.com/736x/9a/d7/06/9ad7065d354a328708fccc8a6b930d30.jpg'
+    },
+    {
+        id:5,
+        name: 'Sets',
+        img: 'https://i.postimg.cc/yNBLVfmN/Set-combo-Reloj-Pulsera-y-Cadena-con-Diamantes-Simulados-Cz-chapado-en-oro-14-K-12.jpg'
+    },
+    {
+        id:6,
+        name: 'Cuelga Gafas',
+        img: 'https://http2.mlstatic.com/D_NQ_NP_721564-MCO43970127309_112020-O.jpg'
+    },
+    {
+        id:7,
+        name: 'Fichas de Alf',
+        img: 'https://www.tierragamer.com/wp-content/uploads/2018/08/ALF_Fichas.jpg'
+    },
+]
+
+
+export const anillos = [{id:null, name:'Un anillo', price:1000, image:'', review:0},{id:null, name:'Un anillo', price:1000, image:'', review:0},{id:null, name:'Un anillo', price:1000, image:'', review:0},{id:null, name:'Un anillo', price:1000, image:'', review:0}]
