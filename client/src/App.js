09bd1137bec88ac652aaf0822a6837f780edbad7
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-06-09 12:51:43 -0300

    changed Catalogo to Catalogue

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import { Home } from './Components/Home';
 import Nav from './Components/Nav/Nav';
 import FormularioAgregarCategoria from "./Components/FormularioAgregarCategoria"
-import Catalogo from "./Components/Catalogo/Catalogo"
+import Catalogue from "./Components/Catalogue/Catalogue"
 import Product from './Components/Product/Product';
 import CreateProduct from "./Components/CreateProduct/CreateProduct"
 
@@ -16,7 +16,7 @@ function App() {
         <Route exact path='/' component={Home}/>
         <Route exact path="/agregarcategoria/" component={FormularioAgregarCategoria}/>
         <Route exact path="/createProduct/" component={ CreateProduct }/>
-        <Route path='/products' component={Catalogo}/>
+        <Route path='/products' component={Catalogue}/>
         <Route path='/products/:id' render={({match}) => <Product match={match}/>}/>
       </Switch>
     </div>
