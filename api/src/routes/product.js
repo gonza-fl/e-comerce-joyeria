9027455add1e7d3f0385d6b3ba1871f973d261a8
=Maximiliano <scriptnoob12@gmail.com>
2021-06-08 21:09:18 -0300

    Route with category

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
-const {Product,Category} = require('../models/index')
+const {Product,Category,Image} = require('../models/index')
 const router = Router();
 
 router.post('/',async(req,res)=>{
     await CreateProduct(req.body,res)
 })
-
 
 const CreateProduct = async(body,res) =>{
     try{
@@ -24,26 +23,33 @@ const CreateProduct = async(body,res) =>{
             price,
             stock
         })
-       /*
+       
         for (let c = 0; c < categories.length; c++) {
             const categorie = await Category.findOne({
                 where:{
                     id:categories[c]
                 }
             })
-            await productCreated.addCategory(categorie)
+            await productCreated.addCategory(categorie) 
         }
-        for (let i = 0; i < images.length; i++) {
+        /*for (let i = 0; i < images.length; i++) {
             const imageCreated = await Product.create({
                 url:images.url
             })
             await productCreated.addImage(Image)
         }*/
-        return res.status(201).json(productCreated)
+        const resultado = await Product.findOne({
+            where:{
+                id:productCreated.id
+            },
+            include:Category
+        })
+        return res.status(201).json(resultado)
     }
     catch(err){
-        return res.status(400);
+        return res.status(400).json(err);
     }
 }
+
 
 module.exports = router;
