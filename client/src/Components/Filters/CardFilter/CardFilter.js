9774c73f04092dbd7621c9043c52b1baf0567166
Sebastian <tassone_af@yahoo.com>
2021-06-09 02:05:47 -0300

    Add stylesButton to CardFilter and link it  whit products path

--- client/src/Components/Filters/CardFilter/CardFilter.js
+++ client/src/Components/Filters/CardFilter/CardFilter.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './CardFilter.css'
+import StyledButton from '../../../StyledComponents/Button';
 
 
-export default function CardFilter({id, name, img}){
+export default function CardFilter({ id, name, img }) {
 
     return (
         <div>
             <div>
-                <p>{name}</p>
-            </div>
-            <Link to={'#'+id}> 
                 <img src={img} alt={name} />
+            </div>
+            <Link to={'/products'}>
+                <StyledButton text={name} />
             </Link>
         </div>
     )
