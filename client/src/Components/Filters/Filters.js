90ae46ba1d2d08eec176728e5594401c7ff6c418
Sebastian <tassone_af@yahoo.com>
2021-06-09 01:24:52 -0300

    Filters component map a cards by category

--- client/src/Components/Filters/Filters.js
+++ client/src/Components/Filters/Filters.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Filters.css';
+import { categoriesF } from '../fakeDB-Categories';
+import CardFilter from './CardFilter/CardFilter';
 
-export default function Filters(){
+export default function Filters() {
+    const [categories, setCategories] = useState([]);
 
-    return(
+    useEffect(() => {
+        //posible axios ----> acá <----
+        setCategories(categoriesF);
+    }, []);
+
+    return (
         <div>
-        
+            {categories.map(cat =>
+                <CardFilter id={cat.id} name={cat.name} img={cat.img} />)}
         </div>
     )
 }
--- client/src/Components/fakeDB-Categories.js
+++ client/src/Components/fakeDB-Categories.js
@@ -1,4 +1,4 @@
-export const categories = [
+export const categoriesF = [
     {
         id:1,
         name: 'Anillos',
