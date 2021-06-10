addc1fcb9cad4202bf55282b225c6dacc64d4a98
Sebastian <tassone_af@yahoo.com>
2021-06-10 19:27:58 -0300

    Catalogue component has filter

--- client/src/Components/Catalogue/Catalogue.jsx
+++ client/src/Components/Catalogue/Catalogue.jsx
@@ -5,15 +5,15 @@ import ProductCard from "../ProductCard/ProductCard"
 import FilterCatalogue from './FilterCatalogue/FilterCatalogue';
 
 export default function Catalogue() {
-    const products = useSelector((state) => state.products);
+  const products = useSelector((state) => state.products);
 
-    const [productsDisplay,setProductsDisplay] = useState(products);
+  const [productsDisplay, setProductsDisplay] = useState(products);
 
   return (
     <div className="catalogo">
       <FilterCatalogue products={productsDisplay} />
       {productsDisplay.map(product => {
-          return <ProductCard name={product.name} price={product.price} id={product.id} image={product.image} review={product.review}></ProductCard>
+        return <ProductCard name={product.name} price={product.price} id={product.id} image={product.image} review={product.review}></ProductCard>
       })}
     </div>
   );
