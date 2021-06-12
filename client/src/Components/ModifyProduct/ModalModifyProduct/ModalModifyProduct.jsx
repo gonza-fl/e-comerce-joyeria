daa86aec4b9d18b42e09b2bd9a363a1e2a10a06a
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-06-12 18:57:59 -0300

    pase por props el id y props.id

--- client/src/Components/ModifyProduct/ModalModifyProduct/ModalModifyProduct.jsx
+++ client/src/Components/ModifyProduct/ModalModifyProduct/ModalModifyProduct.jsx
@@ -4,7 +4,7 @@ import "./modalModifyProduct.css"
 
 
 
-export default function ModalModifyProduct(id){
+export default function ModalModifyProduct(props){
     var modal = document.getElementById("myModal");
     window.onclick = function(event) {
         if (event.target == modal) {
@@ -17,7 +17,7 @@ export default function ModalModifyProduct(id){
         <div id="myModal" className="modal">
           <div className="modal-content">
             <span className="close" onClick={() => {const modal = document.getElementById("myModal"); modal.style.display = "none";}}>X</span>
-            <ModifyProduct id={id}> </ModifyProduct>
+            <ModifyProduct id={props.id}> </ModifyProduct>
           </div>
         </div>
       </div>
