ae0dd6675b4eea8d7c88b03951f28897260b982e
Sebastian <tassone_af@yahoo.com>
2021-06-11 22:05:39 -0300

    add msg for  when filter dont return results

--- client/src/Components/Catalogue/Catalogue.jsx
+++ client/src/Components/Catalogue/Catalogue.jsx
@@ -7,12 +7,13 @@ import FilterCatalogue from './FilterCatalogue/FilterCatalogue';
 export default function Catalogue() {
   const products = useSelector((state) => state.products);
 
-  const [productsDisplay, setProductsDisplay] = useState(products);
+  const [productsDisplay, setProductsDisplay] = useState([...products]);
 
   return (
     <div className="catalogue">
       <FilterCatalogue products={productsDisplay} setProducts={setProductsDisplay} />
       <div className='catalogueMap'>
+        {!productsDisplay.length ?  <h1>Lo siento, no se encontraron coincidencias</h1> : null}
         {productsDisplay.map(product => {
           return <ProductCard product={product} name={product.name} price={product.price} id={product.id} image={product.image} review={product.review}></ProductCard>
         })}
