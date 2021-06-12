2faf3636950c648131a3200b27744c4e1ed6b9c7
Sebastian <tassone_af@yahoo.com>
2021-06-12 18:26:01 -0300

    edit fake data base

--- client/src/Components/fakeDB-Categories.js
+++ client/src/Components/fakeDB-Categories.js
@@ -39,11 +39,11 @@ export const categoriesF = [
 ]
 
 
-const anillos = [{ id: 1, name: 'Un anillo', price: '1002', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '2' },
-{ id: 2, name: 'anillo 1', price: '1000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '3' },
-{ id: 3, name: 'anillo 2', price: '150', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' },
-{ id: 4, name: 'Un anillo mas', price: '10000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' },
-{ id: 5, name: 'anillos', price: '4000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' }
+const manillas = [{ id: 1, name: 'Un manillas', price: '1002', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '2' },
+{ id: 2, name: 'manillas 1', price: '1000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '3' },
+{ id: 3, name: 'manillas 2', price: '150', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' },
+{ id: 4, name: 'Un anilmanillaslo mas', price: '10000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' },
+{ id: 5, name: 'manillas', price: '4000', image: ['https://bodasyweddings.com/wp-content/uploads/2019/04/orden-de-los-anillos-de-boda-y-de-compromiso.jpg'], review: '5' }
 ]
 const collares = [{ id: 2, name: 'collar1234', price: '1000', image: ['https://static.ella-hoy.es/ellahoy/fotogallery/780X0/37755/collares-verano-2012-dorado-con-colgantes.jpg'], review: '3' },
 { id: 2, name: 'Un collar', price: '200', image: ['https://static.ella-hoy.es/ellahoy/fotogallery/780X0/37755/collares-verano-2012-dorado-con-colgantes.jpg'], review: '3' },
@@ -69,15 +69,10 @@ const sets = [{ id: 11, name: 'Un set', price: '1000', image: ['https://i.postim
 { id: 15, name: 'Un set3', price: '1000', image: ['https://i.postimg.cc/yNBLVfmN/Set-combo-Reloj-Pulsera-y-Cadena-con-Diamantes-Simulados-Cz-chapado-en-oro-14-K-12.jpg'], review: '2' },
 { id: 16, name: 'Un set11', price: '4563', image: ['https://i.postimg.cc/yNBLVfmN/Set-combo-Reloj-Pulsera-y-Cadena-con-Diamantes-Simulados-Cz-chapado-en-oro-14-K-12.jpg'], review: '4' }
 ]
-const cuelgaGafas = [{ id: 17, name: 'Un Cuelga Gafas', price: '1000', image: ['https://http2.mlstatic.com/D_NQ_NP_721564-MCO43970127309_112020-O.jpg'], review: '2' },
-{ id: 18, name: 'Un Cuelga Gafas', price: '1000', image: ['https://http2.mlstatic.com/D_NQ_NP_721564-MCO43970127309_112020-O.jpg'], review: '3' },
+const otros = [{ id: 17, name: 'Un Cuelga Gafas Master 2000', price: '1000', image: ['https://http2.mlstatic.com/D_NQ_NP_721564-MCO43970127309_112020-O.jpg'], review: '2' },
+{ id: 18, name: 'Un Cuelga Gafas Sputnik', price: '1000', image: ['https://http2.mlstatic.com/D_NQ_NP_721564-MCO43970127309_112020-O.jpg'], review: '3' },
 
 ]
 
-const alfs = [{ id: 19, name: 'Un Alf', price: '1000', image: ['https://www.tierragamer.com/wp-content/uploads/2018/08/ALF_Fichas.jpg'], review: '5' },
-{ id: 20, name: 'Un Alf', price: '3009', image: ['https://www.tierragamer.com/wp-content/uploads/2018/08/ALF_Fichas.jpg'], review: '5' },
-{ id: 21, name: 'Un Alf', price: '1002', image: ['https://www.tierragamer.com/wp-content/uploads/2018/08/ALF_Fichas.jpg'], review: '4' },
 
-]
-
-export const filtrado = [anillos, collares, aretes, earcuffs, sets, cuelgaGafas, alfs];
+export const filtrado = [aretes, collares, earcuffs,manillas, sets, otros ];
