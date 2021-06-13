9c238e57414e2f063f595ad940415e2c5389bbac
Sebastian <tassone_af@yahoo.com>
2021-06-13 04:51:32 -0300

    Now CardFilter dispatch accion getPrductsById when use clicked it

--- client/src/Components/Filters/CardFilter/CardFilter.js
+++ client/src/Components/Filters/CardFilter/CardFilter.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './CardFilter.css'
 import StyledButton from '../../../StyledComponents/Button';
+import { useDispatch } from 'react-redux';
+import { getProdutsByCategory } from '../../../actions/actions';
 
 
 export default function CardFilter({ id, name, img }) {
 
+    const dispatch = useDispatch();
+
     return (
-        <div className='ctnCardC'>
-            <a href={'/products?' + name}>
+        <div className='ctnCardC' onClick={() => dispatch(getProdutsByCategory(id))}>
+            <Link to={'/products?' + name}>
                 <div className='headerCard'>
                     <img src={img} alt={name} />
                 </div>
                 <StyledButton text={name} />
-            </a>
+            </Link>
         </div>
     )
 }
