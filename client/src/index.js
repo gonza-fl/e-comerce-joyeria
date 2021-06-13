0228688ff818b3da67fc3115e51ddb0d1883a93c
Sebastian <tassone_af@yahoo.com>
2021-06-13 05:05:01 -0300

    configured store for use Redux-Persist

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from "react-redux";
-import store from "./store/store.js";
+import { store, persistor } from "./store/store.js";
+
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+
+      <Router>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Router>
+
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
