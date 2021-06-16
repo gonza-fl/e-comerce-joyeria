69f06b753bce9a524495048a90e2e52ff3f2c96c
Rodrigo <rodrigo.jurado16@gmail.com>
2021-06-16 14:00:48 -0300

    feat(api/order): add GET /order/:id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,11 +4,13 @@ const {
 } = require('express');
 const categoryRoutes = require('./category');
 const product = require('./product');
+const orderRoutes = require('./orders');
 
 const router = Router();
 
 router.use('/products', product);
 router.use('/category', categoryRoutes);
+router.use('/orders', orderRoutes);
 
 router.get('/', (_req, res) => {
   res.send('Soy la Ruta principal');
