87c44b130b11bee60954fc23334a8a1577ad9cc7
dfvallejosc <dfvallejosc@gmail.com>
2021-06-16 10:19:56 -0500

    admin panel

---
+++ client/src/Components/admin/AdminStatistics/AdminStatistics.jsx
@@ -0,0 +1,12 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import React from 'react';
+
+function AdminStatistics() {
+  return (
+    <div>
+      <h1>Aquí se van a mostrar las estadísticas</h1>
+    </div>
+  );
+}
+
+export default AdminStatistics;
