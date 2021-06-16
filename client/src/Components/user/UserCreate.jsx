1f1bf6eb607434628c9cc2046ff22acfab9292dc
Sebastian <tassone_af@yahoo.com>
2021-06-16 10:35:41 -0300

    add styles to form

--- client/src/Components/user/UserCreate.jsx
+++ client/src/Components/user/UserCreate.jsx
@@ -10,11 +10,12 @@
 import React, { useEffect, useState } from 'react';
 import './UserCreate.css';
 import swal from 'sweetalert';
+import Button from '../StyledComponents/Button';
 
 export default function UserCreate() {
   const [submit, setSubmit] = useState(false);
   const [form, setForm] = useState({
-    email: '', name: '', password: '', passwordConfirmation: '', document: '', date: '', genre: 'Seleccionar',
+    email: '', name: '', password: '', passwordConfirmation: '', date: '', genre: 'Seleccionar',
   });
 
   const [errors, setErrors] = useState({
@@ -32,14 +33,12 @@ export default function UserCreate() {
     if (e.target.name === 'passwordConfirmation')
       setErrors(e.target.value === form.password ? { ...errors, password: false } : { ...errors, password: true });
 
-    if (e.target.name === 'password') {
+    if (e.target.name === 'email') {
       const reg = new RegExp('^[^@]+@[^@]+\.[a-zA-Z]{2,}$');
       setErrors({ ...errors, email: !reg.test(form.email) });
     }
   };
-
   const handleSubmit = (e) => {
-    console.log(form);
 
     e.preventDefault();
     setSubmit(true);
@@ -48,50 +47,67 @@ export default function UserCreate() {
       swal('Exito', 'Usuario fue creado con exito', 'success');
     } else
       swal('Error', 'Se produjo un error, por favor verifique los datos', 'warning');
-
+      // "swal-button swal-button--confirm"
   };
 
   return (
-    <div>
-      <p>Bienvenido a K-Mora. Regístrate y sé parte de nuestra tienda online.</p>
+    <div className="formUserCreate">
+      <h2>Bienvenido a K-Mora</h2>
+      <p>Regístrate y sé parte de nuestra tienda online.</p>
 
-      <p><pan className="require">*</pan>Campos Obligatorios</p>
+      <span><span className="require">*</span>Campos Obligatorios</span>
       <form id="formUserCreate" onSubmit={handleSubmit}>
-        <label>E-mail<pan className="require">*</pan> </label>
-        <input name="email" type="email" placeholder="Ingrese su E-mail..." required onChange={handleInputChange} />
-        {submit && errors.email && <pan className="requireMsg">El email ingresado es invalido</pan>}
+        <div>
+          <label>E-mail<span className="require">*</span> </label>
+          <input name="email" type="email" placeholder="Ingrese su E-mail..." required onChange={handleInputChange} />
+          <span className={submit && errors.email ? 'requireMsg' : 'transparent'}>El email ingresado es invalido</span>
+        </div>
+        <div>
+          <label>Contrseña<span className="require">*</span> </label>
+          <input name="password" type="password" placeholder="Ingrese su Contrseña..." required minlength="6" onChange={handleInputChange} />
+        </div>
 
-        <label>Contrseña<pan className="require">*</pan> </label>
-        <input name="password" type="password" placeholder="Ingrese su Contrseña..." required minlength="6" onChange={handleInputChange} />
+        <div>
+          <label>Confirmar Contraseña<span className="require">*</span> </label>
+          <input name="passwordConfirmation" type="password" placeholder="Confirme su Contraseña..." required minlength="6" onChange={handleInputChange} />
+          <span className={errors.password ? 'requireMsg' : 'transparent'}>Las contraseñas no coinciden</span>
+        </div>
 
-        <label>Confirmar Contraseña<pan className="require">*</pan> </label>
-        <input name="passwordConfirmation" type="password" placeholder="Confirme su Contraseña..." required minlength="6" onChange={handleInputChange} />
-        {errors.password && <pan className="requireMsg">Las contraseñas no coinciden</pan>}
+        <div>
+          <label>Nombre<span className="require">*</span></label>
+          <input name="name" placeholder="Ingrese su Nombre..." required onChange={handleInputChange} />
+        </div>
 
-        <label>Nombre<pan className="require">*</pan></label>
-        <input name="name" placeholder="Ingrese su Nombre..." required onChange={handleInputChange} />
+        <div>
+          <label>Apellido<span className="require">*</span> </label>
+          <input name="lastname" placeholder="Ingrese su Apellido..." required onChange={handleInputChange} />
+        </div>
 
-        <label>Apellido<span className="require">*</span> </label>
-        <input name="lastname" placeholder="Ingrese su Apellido..." required onChange={handleInputChange} />
+        <div>
+          <label>Fecha de Nacimiento<span className="require">*</span> </label>
+          <input name="date" type="date" required onChange={handleInputChange} />
+        </div>
 
-        <label>DNI<pan className="require">*</pan> </label>
-        <input name="document" placeholder="Ingrese su DNI..." required onChange={handleInputChange} />
+        <div>
+          <label>Género<span className="require">*</span></label>
+          <select name="genre" onChange={handleInputChange}>
+            <option>Seleccionar</option>
+            <option>Masculino</option>
+            <option>Femenino</option>
+            <option>Sin Género</option>
+          </select>
+          <span className={submit && form.genre === 'Seleccionar' ? 'requireMsg' : 'transparent'}>Seleccione un género</span>
+        </div>
 
-        <label>Fecha de Nacimiento<pan className="require">*</pan> </label>
-        <input name="date" type="date" required onChange={handleInputChange} />
-
-        <label>Género<pan className="require">*</pan></label>
-        <select name="genre" onChange={handleInputChange}>
-          <option>Seleccionar</option>
-          <option>Masculino</option>
-          <option>Femenino</option>
-          <option>Sin Género</option>
-        </select>
-        {submit && form.genre === 'Seleccionar' && <pan className="requireMsg">Seleccione un género</pan>}
-
-        <input type="button" value="Regresar" onClick={() => window.history.back()} />
-        <input type="submit" value="Resgistrarme" />
+        <div>
+          <input className="btnForm  btnBack" type="button" value="Regresar" onClick={() => window.history.back()} />
+          <input className="btnForm font-color-four" type="submit" value="Crear Cuenta" />
+        </div>
       </form>
+      <div>
+        <p>¿Ya tienes un usuario?</p>
+        <Button text="Iniciar Sesion" />
+      </div>
     </div>
   );
 }
