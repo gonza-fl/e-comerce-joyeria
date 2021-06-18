001770e566c6d9c88addf145341d48e9806d383a
Sebastian <tassone_af@yahoo.com>
2021-06-18 02:00:52 -0300

    re config firebase

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,27 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { FirebaseAppProvider } from 'reactfire';
+import firebase from 'firebase/app';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store/store';
 import firebaseConfig from './Firebase';
 
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
-  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-    <Provider store={store}>
-      <Router>
-        <React.StrictMode>
-          <Suspense fallback="Cargando...">
-            <App />
-          </Suspense>
-        </React.StrictMode>
-      </Router>
-    </Provider>
-  </FirebaseAppProvider>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
 reportWebVitals();
