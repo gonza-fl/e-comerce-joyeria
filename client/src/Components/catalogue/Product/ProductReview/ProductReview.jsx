60f55dbdacf1b9b4f5049628e3a567a4c44efdd9
jessicasaule <jessicasaule@gmail.com>
2021-06-24 17:16:59 -0300

    feat(client/ProductReview): added style to the component

--- client/src/Components/catalogue/Product/ProductReview/ProductReview.jsx
+++ client/src/Components/catalogue/Product/ProductReview/ProductReview.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-max-props-per-line */
+/* eslint-disable react/jsx-props-no-multi-spaces */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
@@ -6,8 +8,13 @@ import React from 'react';
 import ReactStars from 'react-rating-stars-component';
 // import axios from 'axios';
 // import { URL_GET_REVIEWS } from '../../../../constants';
+import './ProductReview.css';
 
 const ProductReview = ({ productId }) => {
+  // cuando se haga la conexion con el back descomentar todo y borrar el array reviewTest
+  // en la renderizacion cambiar 'reviewTest' por 'review'
+  // no olvidar importar useState y useEffect
+
   // const [review, setReview] = useState([]);
 
   // useEffect(() => {
@@ -29,10 +36,10 @@ const ProductReview = ({ productId }) => {
       id: 3, calification: 4, description: 'Cuando un joven era él, (¡Cuando joven era yo!), Muy bien, (Gracias)',
     },
     {
-      id: 4, calification: 1, description: 'Sintió que su aroma le dio mucha fama, vació la sabana, después de comer, Un alma sensible soy, aunque de cuero cubierto estoy, y a mis amigos, el viento se los llevó',
+      id: 4, calification: 2, description: 'Sintió que su aroma le dio mucha fama, vació la sabana, después de comer, Un alma sensible soy, aunque de cuero cubierto estoy, y a mis amigos, el viento se los llevó',
     },
     {
-      id: 5, calification: 2, description: 'Qué vergüenza, (oh, qué vergüenza), Mi nombre cambió a Hortencia, (Su nombre no le queda), Y mucho sufrí yo, (ay, cómo sufrió), Cada vez que yo, ¡Pumba!, no en frente de los niños! ¡Oh!, perdón',
+      id: 5, calification: 1, description: 'Qué vergüenza, (oh, qué vergüenza), Mi nombre cambió a Hortencia, (Su nombre no le queda), Y mucho sufrí yo, (ay, cómo sufrió), Cada vez que yo, ¡Pumba!, no en frente de los niños! ¡Oh!, perdón',
     },
     {
       id: 6, calification: 4, description: 'Hakuna matata, Una forma de ser, Hakuna matata',
@@ -42,11 +49,11 @@ const ProductReview = ({ productId }) => {
   const average = reviewTest.map((review) => review.calification).reduce((a, b) => a + b, 0) / reviewTest.length;
 
   return (
-    <div>
-      <h2>Opiniones sobre el producto</h2>
-      <div>
-        <p>{average.toFixed(1)}</p>
-        <div>
+    <div className="product-reviews-container">
+      <h2 id="product-reviews-container-title">Opiniones sobre el producto</h2>
+      <div className="average-review">
+        <p id="average-number">{average.toFixed(1)}</p>
+        <div className="average-review-stars">
           <ReactStars
             count={5}
             size={24}
@@ -54,12 +61,12 @@ const ProductReview = ({ productId }) => {
             value={average}
             activeColor="#ffd700"
           />
-          <p>Promedio entre {reviewTest.length} opiniones</p>
+          <p id="average-declaration">Promedio entre {reviewTest.length} opiniones</p>
         </div>
       </div>
-      <div>
+      <div className="description-reviews-container">
         {reviewTest.map((review) => (
-          <div>
+          <div className="description-review">
             <ReactStars
               count={5}
               size={24}
@@ -67,7 +74,7 @@ const ProductReview = ({ productId }) => {
               value={review.calification}
               activeColor="#ffd700"
             />
-            <p>{review.description}</p>
+            <p id="description-review-description">{review.description}</p>
           </div>
         ))}
       </div>
