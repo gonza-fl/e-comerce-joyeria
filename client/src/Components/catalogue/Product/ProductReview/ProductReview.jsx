14884c7a27e86a4932e8eaa3e98a298bcd817244
jessicasaule <jessicasaule@gmail.com>
2021-06-24 19:47:40 -0300

    (client/ProductReview): connected component with back end route. ANDA

--- client/src/Components/catalogue/Product/ProductReview/ProductReview.jsx
+++ client/src/Components/catalogue/Product/ProductReview/ProductReview.jsx
@@ -4,77 +4,54 @@
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactStars from 'react-rating-stars-component';
-// import axios from 'axios';
-// import { URL_GET_REVIEWS } from '../../../../constants';
+import axios from 'axios';
+import swal from 'sweetalert';
+import { URL_PRODUCTS } from '../../../../constants';
 import './ProductReview.css';
 
 const ProductReview = ({ productId }) => {
-  // cuando se haga la conexion con el back descomentar todo y borrar el array reviewTest
-  // en la renderizacion cambiar 'reviewTest' por 'review'
-  // no olvidar importar useState y useEffect
+  const [review, setReview] = useState([]);
 
-  // const [review, setReview] = useState([]);
+  useEffect(() => {
+    axios.get(`${URL_PRODUCTS}${productId}/review`)
+      .then((response) => {
+        setReview(response.data);
+      })
+      .catch((err) => swal('Error', err.response.data, 'warning'));
+  }, []);
 
-  // useEffect(() => {
-  //   axios.get(`${URL_GET_REVIEWS}${productId}/review`)
-  //     .then((response) => {
-  //       setReview(response.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
-  const reviewTest = [
-    {
-      id: 1, calification: 5, description: 'Hakuna matata, una forma de ser, Hakuna matata, nada que temer',
-    },
-    {
-      id: 2, calification: 3, description: 'Sin preocuparse es como hay que vivir, a vivir así, yo aquí aprendí, Hakuna matata',
-    },
-    {
-      id: 3, calification: 4, description: 'Cuando un joven era él, (¡Cuando joven era yo!), Muy bien, (Gracias)',
-    },
-    {
-      id: 4, calification: 2, description: 'Sintió que su aroma le dio mucha fama, vació la sabana, después de comer, Un alma sensible soy, aunque de cuero cubierto estoy, y a mis amigos, el viento se los llevó',
-    },
-    {
-      id: 5, calification: 1, description: 'Qué vergüenza, (oh, qué vergüenza), Mi nombre cambió a Hortencia, (Su nombre no le queda), Y mucho sufrí yo, (ay, cómo sufrió), Cada vez que yo, ¡Pumba!, no en frente de los niños! ¡Oh!, perdón',
-    },
-    {
-      id: 6, calification: 4, description: 'Hakuna matata, Una forma de ser, Hakuna matata',
-    },
-  ];
-
-  const average = reviewTest.map((review) => review.calification).reduce((a, b) => a + b, 0) / reviewTest.length;
+  const average = review.map((rev) => rev.calification).reduce((a, b) => a + b, 0) / review.length;
+  const roundAverage = average.toFixed(1);
 
   return (
     <div className="product-reviews-container">
       <h2 id="product-reviews-container-title">Opiniones sobre el producto</h2>
       <div className="average-review">
-        <p id="average-number">{average.toFixed(1)}</p>
+        <p id="average-number">{roundAverage}</p>
         <div className="average-review-stars">
           <ReactStars
             count={5}
             size={24}
             edit={false}
-            value={average}
+            value={Math.round(average)}
             activeColor="#ffd700"
           />
-          <p id="average-declaration">Promedio entre {reviewTest.length} opiniones</p>
+          <p id="average-declaration">Promedio entre {review.length} opiniones</p>
         </div>
       </div>
       <div className="description-reviews-container">
-        {reviewTest.map((review) => (
+        {review.map((rev) => (
           <div className="description-review">
             <ReactStars
               count={5}
               size={24}
               edit={false}
-              value={review.calification}
+              value={rev.calification}
               activeColor="#ffd700"
             />
-            <p id="description-review-description">{review.description}</p>
+            <p id="description-review-description">{rev.description}</p>
           </div>
         ))}
       </div>
