686f6d9f78803e357338e4131798e408e7bf6ae5
Sebastian <tassone_af@yahoo.com>
2021-06-25 21:45:25 -0300

    .

--- client/src/Components/user/Profile/EditPassword/EditPassword.jsx
+++ client/src/Components/user/Profile/EditPassword/EditPassword.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
 import './EditPassword.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 export default function EditPassword() {
-  const user = firebase.auth().currentUser;
-  console.log(user.providerData);
+  // const user = firebase.auth().currentUser;
   // if (user) user.updatePassword('123123').then(() => alert('yes'));
 
+  const handleClick = () => {
+    firebase.auth().sendPasswordResetEmail('tassone_af@yahoo.com').then(alert('yes)'));
+  };
   return (
-    <div />
+    <div>
+      <button onClick={handleClick}>click</button>
+    </div>
   );
 }
