b0dd0c84d93101c5df47cd7349f1e0c43137cde5
Gonza Flamenci <gonzaloflamenci4@gmail.com>
2021-06-26 19:07:34 -0300

    Testing config 3

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -5,6 +5,7 @@ const {
 const {
   dbUser,
   dbName,
+  dbNameTest,
   dbHost,
   dbPassword,
 } = require('../utils/config/index');
@@ -18,7 +19,7 @@ const UserFactory = require('./User');
 const OrderLineFactory = require('./OrderLine');
 const ReviewFactory = require('./Reviews');
 
-const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${dbName}`, {
+const sequelize = new Sequelize(`postgres://${dbUser}:${dbPassword}@${dbHost}/${process.env.NODE_ENV === 'test' ? dbName : dbNameTest}`, {
   logging: false,
 });
 
--- api/src/utils/config/index.js
+++ api/src/utils/config/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
   dbUser: process.env.DB_USER || 'postgres',
   dbName: process.env.DB_NAME || 'joyas',
+  dbNameTest: process.env.DB_NAME_TEST || 'joyas_test',
   dbPort: process.env.DB_PORT || '5432',
   dbHost: process.env.HOST || 'localhost',
   dbPassword: process.env.DB_PASSWORD || 'puey1017',
