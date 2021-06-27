95698e148b0def0a06a643153617953fda731011
Rodrigo <rodrigo.jurado16@gmail.com>
2021-06-27 20:03:39 -0300

    fix (api/routes): POST/user checks if id or displayName is empty

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -7,10 +7,16 @@ const {
 
 const createUser = async (req, res) => {
   const birthday = req.body.birthday && req.body.birthday.split('-');
-  const {
-    id, email, displayName, phone,
-
+  let {
+    id, displayName,
   } = req.body;
+  const {
+    email, phone,
+  } = req.body;
+  if (!id || !id.trim().length) return res.status(400).send('Id incorrecto');
+  if (!displayName || !displayName.trim().length) return res.status(400).send('displayName incorrecto');
+  displayName = displayName.trim();
+  id = id.trim();
   const birthdayNew = birthday ? new Date(birthday[2], birthday[1] - 1, birthday[0]) : null;
   try {
     await User.create({
@@ -23,7 +29,7 @@ const createUser = async (req, res) => {
     });
     return res.status(201).send('Usuario creado correctamente!');
   } catch (err) {
-    return res.status(500).send('Internal server error');
+    return res.status(500).send('Internal server error. No se creó el usuario');
   }
 };
 
