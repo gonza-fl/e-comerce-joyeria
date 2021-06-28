4b5163ca7b39261106b0fb3428d4958798205fdf
Sebastian <tassone_af@yahoo.com>
2021-06-27 21:44:05 -0300

    responsive catalogur from tablet

--- client/src/Components/catalogue/ProductCard/ProductCard.jsx
+++ client/src/Components/catalogue/ProductCard/ProductCard.jsx
@@ -106,14 +106,15 @@ const DivCard = styled.div`
             transition: transform 300ms;
           }
 
-          @media (max-width: 452px) {
-            img {
-              width:130px;
-              height:130px
+
+            @media (max-width: 768px) {
+              img {
+                width:130px;
+                height:130px
+              }
+              padding:0;
+              width: 40%;
             }
-            padding:0;
-            width: 40%;
-          }
 `;
 
 const DivCarousel = styled.div`
