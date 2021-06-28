55b0823d14f9d886cd178f667d175bfbab8385f1
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-06-28 09:33:24 -0300

    panel admin, navBar responsive 100%

--- client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
+++ client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
@@ -5,12 +5,12 @@ import './adminNavBar.css';
 const AdminNavBar = () => (
   <div className="container">
 
-    <Link to="/" className="link-without-styles"><div>VOLVER A INICIO</div></Link>
-    <Link to="/admin/products" className="link-without-styles"><div>VER PRODUCTOS</div></Link>
-    <Link to="/admin/controlcategories" className="link-without-styles"><div>CATEGORÍAS</div></Link>
-    <Link to="/admin/statistics" className="link-without-styles"><div>VER ESTADÍSTICAS</div></Link>
-    <Link to="/admin/orders" className="link-without-styles"><div>VER VENTAS</div></Link>
-    <Link to="/admin/users" className="link-without-styles"><div>USUARIOS</div></Link>
+    <Link to="/" className="link-without-styles"><div className="linkedDiv">VOLVER A INICIO</div></Link>
+    <Link to="/admin/products" className="link-without-styles"><div className="linkedDiv">VER PRODUCTOS</div></Link>
+    <Link to="/admin/controlcategories" className="link-without-styles"><div className="linkedDiv">CATEGORÍAS</div></Link>
+    <Link to="/admin/statistics" className="link-without-styles"><div className="linkedDiv">VER ESTADÍSTICAS</div></Link>
+    <Link to="/admin/orders" className="link-without-styles"><div className="linkedDiv">VER VENTAS</div></Link>
+    <Link to="/admin/users" className="link-without-styles"><div className="linkedDiv">USUARIOS</div></Link>
 
   </div>
 );
