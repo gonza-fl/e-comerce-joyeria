3b705a0137a1ba5b12ddc142db93e54baca060e4
Gonza Flamenci <gonzaloflamenci4@gmail.com>
2021-06-29 16:22:40 -0300

    Change acces

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ server.use(express.json({
 server.use(morgan('dev'));
 server.use(cors());
 server.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
