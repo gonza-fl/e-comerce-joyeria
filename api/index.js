ae077666c2cd9b68746cfafcd3102bbe76e3ae1d
nohaynicksdisponibles <jonathanezequielsosa@hotmail.com>
2021-06-30 10:11:53 -0300

    agregado header

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ server.use(cors());
 server.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, access-token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
