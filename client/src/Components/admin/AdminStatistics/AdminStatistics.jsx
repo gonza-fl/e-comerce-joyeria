2333984e947eeaabaea81a01b3b437863f015553
jessicasaule <jessicasaule@gmail.com>
2021-06-30 00:37:55 -0300

    feat(client/AdminStatistics): gave structure and style to component

--- client/src/Components/admin/AdminStatistics/AdminStatistics.jsx
+++ client/src/Components/admin/AdminStatistics/AdminStatistics.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './AdminStatistics.css';
 
 function AdminStatistics() {
   return (
-    <div>
-      <h1>Aquí se van a mostrar las estadísticas</h1>
+    <div className="stats-container">
+      <div className="stats-sales-btn">
+        <button className="sales-btn" type="submit">Ventas por Mes</button>
+        <button className="sales-btn" type="submit">Ventas según Productos</button>
+        <button className="sales-btn" type="submit">Ventas según Usuarios</button>
+        <button className="sales-btn" type="submit">Ventas por Año</button>
+      </div>
+      <div className="stats-chart-container">
+        Gráfico aquí
+      </div>
     </div>
   );
 }
