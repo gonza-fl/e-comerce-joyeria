df003a264e628fb9e833ffc9800108eba32d603b
jessicasaule <jessicasaule@gmail.com>
2021-07-01 00:10:50 -0300

    added functionality to chart and clean chart option

--- client/src/Components/admin/AdminStatistics/AdminStatistics.jsx
+++ client/src/Components/admin/AdminStatistics/AdminStatistics.jsx
@@ -2,9 +2,10 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable no-undef */
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 import swal from 'sweetalert';
+import { getProducts } from '../../../redux/actions/actions';
 import {
   ResponsiveContainer,
   AreaChart,
@@ -14,7 +15,7 @@ import {
   Tooltip,
   CartesianGrid,
 } from 'recharts';
-import { format, parseISO, subDays } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import { URL_GET_STATS } from '../../../constants';
 import './AdminStatistics.css';
 
@@ -22,10 +23,18 @@ function AdminStatistics() {
   const user = useSelector((state) => state.user);
   const [type, setType] = useState('');
   const [statsInfo, setStatsInfo] = useState([]);
-  
+  const [selectedProductId, setSelectedProductId] = useState(0);
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    if (type) {
-      axios.get(`${URL_GET_STATS}${type}`, {
+    dispatch(getProducts());
+  }, []);
+
+  useEffect(() => {
+    if (type === 'totalsPerDate') {
+      const request = {type};
+      axios.post(URL_GET_STATS, request, {
         headers: {
           'access-token': user.id,
         },
@@ -38,10 +47,52 @@ function AdminStatistics() {
         })
         .catch((err) => swal('Error', err.response.data, 'warning'));
     }
-  }, [type]);
+
+    if (type === 'productAmountPerDate' && selectedProductId) {
+      console.log('holis');
+      const request = {type, productId: selectedProductId}
+      axios.post(URL_GET_STATS, request, {
+        headers: {
+          'access-token': user.id,
+        },
+      })
+      .then((response) => {
+        const ordersWithSlicedDates = response.data.map(
+          (order) => { return {...order, endTimestamp: order.endTimestamp.substr(0, 10)
+          }})
+        setStatsInfo(ordersWithSlicedDates);
+      })
+      .catch((err) => swal('Error', err.response.data, 'warning'));
+    }
+
+    if (type === 'totalsPerDateByUsers') {
+      const request = {type, userId: user.id};
+      axios.post(URL_GET_STATS, request, {
+        headers: {
+          'access-token': user.id,
+        },
+      })
+        .then((response) => {
+          const ordersWithSlicedDates = response.data.map(
+            (order) => { return {...order, endTimestamp: order.endTimestamp.substr(0, 10)
+            }})
+          setStatsInfo(ordersWithSlicedDates);
+        })
+        .catch((err) => swal('Error', err.response.data, 'warning'));
+    }
+  }, [type, selectedProductId]);
 
   const handleType = (e) => {
     setType(e.target.value);
+  };
+
+  const handleSelectChange = (e) => {
+    setSelectedProductId(e.target.value);
+  };
+
+  const handleReset = () => {
+    setType('');
+    setStatsInfo([]);
   };
 
   return (
@@ -49,9 +100,17 @@ function AdminStatistics() {
       <div className="stats-sales-btn">
         <button className="sales-btn" value="totalsPerDate" onClick={(e) => handleType(e)}>Ventas por Mes</button>
         <button className="sales-btn" value="productAmountPerDate" onClick={(e) => handleType(e)}>Ventas según Productos</button>
+        {type === 'productAmountPerDate' &&
+        <select onChange={handleSelectChange} >
+          {products.map((product) =>
+            <option value={product.id}>{product.name}</option>
+          )}
+        </select>
+      }
         <button className="sales-btn" value="totalsPerDateByUsers" onClick={(e) => handleType(e)}>Ventas según Usuarios</button>
-        <button className="sales-btn" value="totalsPerDate" onClick={(e) => handleType(e)}>Ventas por Año</button>
+        <button className="sales-btn" onClick={() => handleReset()}>Limpiar</button>
       </div>
+      
       <div className="stats-chart-container">
         {!statsInfo.length ? null : 
           <ResponsiveContainer width="96%" height={500}>
