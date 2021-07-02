25295e674ba274c509e27a8710d16083328bc71a
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-07-02 02:33:35 -0300

    nombres admin nav

--- client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
+++ client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
@@ -9,17 +9,17 @@ const AdminNavBar = () => (
     <ul style={{ listStyle: 'none', textAlign: 'left' }}>
       <Link to="/" className="link-without-styles"><li>VOLVER A INICIO</li></Link>
       <br />
-      <Link to="/admin/products" className="link-without-styles"><li>VER PRODUCTOS</li></Link>
+      <Link to="/admin/products" className="link-without-styles"><li>PRODUCTOS</li></Link>
       <br />
       <Link to="/admin/controlcategories" className="link-without-styles"><li>CATEGORÍAS</li></Link>
       <br />
-      <Link to="/admin/statistics" className="link-without-styles"><li>VER ESTADÍSTICAS</li></Link>
+      <Link to="/admin/statistics" className="link-without-styles"><li>ESTADÍSTICAS</li></Link>
       <br />
-      <Link to="/admin/orders" className="link-without-styles"><li>VER VENTAS</li></Link>
+      <Link to="/admin/orders" className="link-without-styles"><li>VENTAS</li></Link>
       <br />
       <Link to="/admin/users" className="link-without-styles"><li>USUARIOS</li></Link>
       <br />
-      <Link to="/admin/flyers" className="link-without-styles"><li>FLYERS PROMOCIONAL</li></Link>
+      <Link to="/admin/flyers" className="link-without-styles"><li>BANNERS</li></Link>
     </ul>
     {/* <Link to="/" className="link-without-styles"><div className="linkedDiv">VOLVER A INICIO</div></Link>
     <Link to="/admin/products" className="link-without-styles"><div className="linkedDiv">VER PRODUCTOS</div></Link>
