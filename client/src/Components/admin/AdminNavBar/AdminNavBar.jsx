f6c2c7234ebc07f43c8deb07dafdf87a8994a944
Dimitri Triantafilo <dimitritriantafilo@gmail.com>
2021-07-02 03:04:23 -0300

    cambios de diego

--- client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
+++ client/src/Components/admin/AdminNavBar/AdminNavBar.jsx
@@ -1,25 +1,41 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './adminNavBar.css';
 
-const AdminNavBar = () => (
-  <div className="container">
-    <ul style={{ listStyle: 'none', textAlign: 'left' }}>
-      <Link to="/" className="link-without-styles"><li>VOLVER A INICIO</li></Link>
-      <br />
-      <Link to="/admin/products" className="link-without-styles"><li>PRODUCTOS</li></Link>
-      <br />
-      <Link to="/admin/controlcategories" className="link-without-styles"><li>CATEGORÍAS</li></Link>
-      <br />
-      <Link to="/admin/statistics" className="link-without-styles"><li>ESTADÍSTICAS</li></Link>
-      <br />
-      <Link to="/admin/orders" className="link-without-styles"><li>VENTAS</li></Link>
-      <br />
-      <Link to="/admin/users" className="link-without-styles"><li>USUARIOS</li></Link>
-      <br />
-      <Link to="/admin/flyers" className="link-without-styles"><li>BANNERS</li></Link>
-    </ul>
-  </div>
-);
+const AdminNavBar = () => {
+  const [selected, setSelected] = useState(null);
+  return (
+    <div className="container">
+      <ul style={{ listStyle: 'none', textAlign: 'left' }}>
+        <Link to="/" className="link-without-styles"><li>VOLVER A INICIO</li></Link>
+        <br />
+        <Link to="/admin/products" className="link-without-styles" style={{ color: selected === 1 ? 'white' : 'inherit' }} onClick={() => setSelected(1)}>
+          <li>PRODUCTOS</li>
+        </Link>
+        <br />
+        <Link to="/admin/controlcategories" className="link-without-styles" style={{ color: selected === 2 ? 'white' : 'inherit' }} onClick={() => setSelected(2)}>
+          <li>CATEGORÍAS</li>
+        </Link>
+        <br />
+        <Link to="/admin/statistics" className="link-without-styles" style={{ color: selected === 3 ? 'white' : 'inherit' }} onClick={() => setSelected(3)}>
+          <li>ESTADÍSTICAS</li>
+        </Link>
+        <br />
+        <Link to="/admin/orders" className="link-without-styles" style={{ color: selected === 4 ? 'white' : 'inherit' }} onClick={() => setSelected(4)}>
+          <li>VENTAS</li>
+        </Link>
+        <br />
+        <Link to="/admin/users" className="link-without-styles" style={{ color: selected === 5 ? 'white' : 'inherit' }} onClick={() => setSelected(5)}>
+          <li>USUARIOS</li>
+        </Link>
+        <br />
+        <Link to="/admin/flyers" className="link-without-styles" style={{ color: selected === 6 ? 'white' : 'inherit' }} onClick={() => setSelected(6)}>
+          <li>BANNERS</li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
 export default AdminNavBar;
